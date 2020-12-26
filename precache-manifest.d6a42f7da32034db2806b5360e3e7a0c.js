436b4b1055edb4e043aa374ada53551dadd1750d
Kishore <kishor4u@gmail.com>
2020-12-26 18:56:28 -0500

    Updates

---
+++ precache-manifest.d6a42f7da32034db2806b5360e3e7a0c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8c6568b34cb97e8d23f49a9774b4c0",
+    "url": "/iampranav/index.html"
+  },
+  {
+    "revision": "a99e9bc94464a6a3fbcc",
+    "url": "/iampranav/static/css/main.3a65ec82.chunk.css"
+  },
+  {
+    "revision": "163455f5bd2690e51d55",
+    "url": "/iampranav/static/js/2.87c0bcde.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/iampranav/static/js/2.87c0bcde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99e9bc94464a6a3fbcc",
+    "url": "/iampranav/static/js/main.c8a52b9b.chunk.js"
+  },
+  {
+    "revision": "49b74d3caa423e1ba325",
+    "url": "/iampranav/static/js/runtime-main.9169cc41.js"
+  },
+  {
+    "revision": "25d5f08daf1104a18a53142b59b37173",
+    "url": "/iampranav/static/media/background2.25d5f08d.jpg"
+  },
+  {
+    "revision": "19d3f3243680257654910c6118a8871f",
+    "url": "/iampranav/static/media/iampranav.19d3f324.jpg"
+  },
+  {
+    "revision": "3f9ffe403ef53d13e2d151899fb20490",
+    "url": "/iampranav/static/media/picture.3f9ffe40.png"
+  }
+]);
